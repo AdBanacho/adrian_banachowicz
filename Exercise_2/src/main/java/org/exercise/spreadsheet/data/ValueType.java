c8a953da1eac4e29c7c265ba28aa6c1c7ed4a0e0
Adr <adbanachowicz@gmail.com>
2025-01-07 01:14:36 +0100

    Exercise 2: Feature - Implement method to determine a type of cell's value

--- Exercise_2/src/main/java/org/exercise/spreadsheet/data/ValueType.java
+++ Exercise_2/src/main/java/org/exercise/spreadsheet/data/ValueType.java
@@ -3,6 +3,16 @@ package org.exercise.spreadsheet.data;
 public enum ValueType {
     INTEGER,
     FORMULA,
-    STRING
+    STRING;
+
+    public static ValueType determineValueType(String value) {
+        if (value.matches("-?\\d+(\\.\\d+)?")) {
+            return ValueType.INTEGER;
+        } else if (value.startsWith("=")){
+            return ValueType.FORMULA;
+        } else {
+            return ValueType.STRING;
+        }
+    }
 
 }
