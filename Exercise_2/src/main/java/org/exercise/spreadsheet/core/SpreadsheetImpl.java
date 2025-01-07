fc08d4ade9ab13622711737d258e4f47a229e362
Adr <adbanachowicz@gmail.com>
2025-01-07 01:02:23 +0100

    Exercise 2: Feature - Implement method to initialize cells with default values

--- Exercise_2/src/main/java/org/exercise/spreadsheet/core/SpreadsheetImpl.java
+++ Exercise_2/src/main/java/org/exercise/spreadsheet/core/SpreadsheetImpl.java
@@ -3,11 +3,22 @@ package org.exercise.spreadsheet.core;
 import org.exercise.spreadsheet.data.Cell;
 import org.exercise.spreadsheet.data.ValueType;
 
+import java.util.stream.IntStream;
+
 public class SpreadsheetImpl {
     private final Cell[][] cells;
 
     public SpreadsheetImpl(int rows, int columns) {
         this.cells = new Cell[rows][columns];
+        initializeCells(rows, columns);
+    }
+
+    private void initializeCells(int rows, int columns) {
+        IntStream.range(0, rows).forEach(row ->
+                IntStream.range(0, columns).forEach(col ->
+                        this.cells[row][col] = new Cell(ValueType.STRING, "")
+                )
+        );
     }
 
     public String get(int row, int column) {
