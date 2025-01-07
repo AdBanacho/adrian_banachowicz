dac75d03466b523dcd5ac9c0e47d7f0a12bb3e72
Adr <adbanachowicz@gmail.com>
2025-01-07 02:10:33 +0100

    Exercise 2: Feature - Implement method to export spreadsheet

--- Exercise_2/src/main/java/org/exercise/spreadsheet/exporters/AbstractSpreadsheetExporter.java
+++ Exercise_2/src/main/java/org/exercise/spreadsheet/exporters/AbstractSpreadsheetExporter.java
@@ -2,8 +2,10 @@ package org.exercise.spreadsheet.exporters;
 
 import org.exercise.spreadsheet.core.SpreadsheetImpl;
 
+import java.util.stream.IntStream;
+
 abstract class AbstractSpreadsheetExporter {
-    protected final SpreadsheetImpl sheet;
+    private final SpreadsheetImpl sheet;
 
     public AbstractSpreadsheetExporter(SpreadsheetImpl sheet) {
         this.sheet = sheet;
@@ -12,6 +14,21 @@ abstract class AbstractSpreadsheetExporter {
     abstract String getExportOption();
 
     public String export() {
-        return "";
+        int rows = sheet.getNumberOfRows();
+        int columns = sheet.getNumberOfColumns();
+        String exportOption = getExportOption();
+        StringBuilder stringToExport = new StringBuilder();
+        
+        stringToExport.append(rows).append(",").append(columns).append("#");
+
+        IntStream.range(0, rows).forEach(row -> {
+            IntStream.range(0, columns).forEach(col -> {
+                String cellValue = sheet.get(row, col);
+                String exportValue = cellValue == null || cellValue.isEmpty() ? "" : cellValue;
+                stringToExport.append(exportValue).append(exportOption);
+            });
+        });
+
+        return stringToExport.toString();
     }
 }
