1ac76bf5d1f263a1f42b7619253fb20eefd4efd5
Adr <adbanachowicz@gmail.com>
2025-01-08 02:20:34 +0100

    Exercise 3-6: Feature - Implement JSON Loader

--- Exercise_3/src/main/java/org/exercise/dependencyGraph/DependencyGraphImpl.java
+++ Exercise_3/src/main/java/org/exercise/dependencyGraph/DependencyGraphImpl.java
@@ -1,8 +1,16 @@
 package org.exercise.dependencyGraph;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DependencyGraphImpl implements DependencyGraph {
 
@@ -22,5 +30,18 @@ public class DependencyGraphImpl implements DependencyGraph {
         return null;
     }
 
+    private void loadFromJson(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        String content = Files.readString(path);
+        JSONObject json = new JSONObject(content);
 
+        json.keySet().forEach(key -> {
+            Set<String> dependencies = json.getJSONArray(key)
+                    .toList()
+                    .stream()
+                    .map(Object::toString)
+                    .collect(Collectors. toCollection(HashSet::new));
+            graph.put(key, dependencies);
+        });
+    }
 }
